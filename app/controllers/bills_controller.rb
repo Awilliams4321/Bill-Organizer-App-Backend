a4c9ef25d1c8b01efd2e9946d146405b4761bf75
Awilliams4321 <awilliams1234@live.com>
2020-12-16 11:59:42 -0700

    Adding a private method that permits a new Bill object to be created with the given attributes.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -9,4 +9,10 @@ class BillsController < ApplicationController
         bill = Bill.find_by(id: params[:id])
         render json: bill.to_json
     end
+
+    private
+
+    def bill_params
+        params.require(:bill).permit(:name, :creditor, :balance_owed, :monthly_payment, :due_date, :user_id)
+    end
 end
