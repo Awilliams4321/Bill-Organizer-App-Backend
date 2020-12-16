99700af11342f11f0a7b1241a7f21fe132e18b79
Awilliams4321 <awilliams1234@live.com>
2020-12-16 12:33:34 -0700

    Creating an index and show method. Adding a private method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        render json: users.to_json
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user.to_json
+    end
+
+    private
+
+    def bill_params
+        params.require(:user).permit(:name, :email)
+    end
+
 end
