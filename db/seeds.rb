8ba48e03979df0bd567653a51269eb13e26b17eb
Awilliams4321 <awilliams1234@live.com>
2020-12-15 21:09:26 -0700

    Seeding data to my database with 'rake db:seed' command

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: "Test1", email: "tester1@gmail.com")
+User.create(name: "Test2", email: "tester2@gmail.com")
+
+Bill.create(name: "Car Payment", creditor: "Suntrust", balance_owed: 15000, monthly_payment: 250, due_date: 15, user_id: 1)
+Bill.create(name: "School Loan", creditor: "Climb", balance_owed: 11000, monthly_payment: 400, due_date: 25, user_id: 2)
+
