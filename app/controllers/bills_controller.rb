b9bf7e73319bb2b58de5dfc98c7268599bd8485a
Awilliams4321 <awilliams1234@live.com>
2020-12-16 11:54:19 -0700

    Adding a show method that finds a bill by its id.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -4,4 +4,9 @@ class BillsController < ApplicationController
         bills = Bill.all 
         render json: bills.to_json
     end
+
+    def show
+        bill = Bill.find_by(id: params[:id])
+        render json: bill.to_json
+    end
 end
