652f2e784d4f6389b0b2a1326395818bdaf2d378
Awilliams4321 <awilliams1234@live.com>
2020-12-17 10:57:27 -0700

    No longer using the User model in my application so i am deleting all methods and relationships. I am keeping the migrations so that i can work on user authentication at a later date.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,3 @@
 class UsersController < ApplicationController
 
-    def index
-        users = User.all 
-        render json: users.to_json
-    end
-
-    def show
-        user = User.find_by(id: params[:id])
-        render json: user.to_json
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:name, :email)
-    end
-
 end
