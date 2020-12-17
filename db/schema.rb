46fedb693cdbf7ec5e5f07aab75f3b4de9aa0e86
Awilliams4321 <awilliams1234@live.com>
2020-12-17 10:55:43 -0700

    Adding a category_id column to my Bills table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_171941) do
+ActiveRecord::Schema.define(version: 2020_12_17_174836) do
 
   create_table "bills", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_12_17_171941) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_bills_on_category_id"
     t.index ["user_id"], name: "index_bills_on_user_id"
   end
 
@@ -37,5 +39,6 @@ ActiveRecord::Schema.define(version: 2020_12_17_171941) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bills", "categories"
   add_foreign_key "bills", "users"
 end
