1a3be17f0c54aac0f3a4eb66a3c98a8dd1a26bc8
Awilliams4321 <awilliams1234@live.com>
2020-12-24 10:57:31 -0700

    Updating my create method.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
     end
 
     def create
-        category.new(category_params)
+        category = Category.new(category_params)
         if category.save
             render json: CategorySerializer.new(category)
         else
