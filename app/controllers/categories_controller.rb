9504498f3b4d672f64a7e0f2e5dbffa124192d06
Awilliams4321 <awilliams1234@live.com>
2020-12-24 10:47:20 -0700

    Adding a create method which passes in the private category_params method when creating a new category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,5 +10,19 @@ class CategoriesController < ApplicationController
         render json: CategorySerializer.new(category)
     end
 
+    def create
+        category.new(category_params)
+        if category.save
+            render json: CategorySerializer.new(category)
+        else
+            render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
 
 end
