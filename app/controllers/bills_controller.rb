7d90e803aefb477c82ad326a8cfa2b152d5268d1
Awilliams4321 <awilliams1234@live.com>
2020-12-30 17:05:57 -0700

    Updating my create method that will create a new bill if it is saved and return an error if it cannot be.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,17 +2,27 @@ class BillsController < ApplicationController
 
     def index
         bills = Bill.all 
-        render json: bills.to_json
+        render json: BillSerializer.new(bills)
     end
 
     def show
         bill = Bill.find_by(id: params[:id])
-        render json: bill.to_json
+        render json: BillSerializer.new(bill)
+    end
+
+    def create
+        byebug
+        bill = Bill.new(bill_params)
+        if bill.save
+            render json: BillSerializer.new(bill)
+        else
+            render json: {errors: bill.errors.full_messages}, status: :unprocessible_entity
+        end
     end
 
     private
 
     def bill_params
-        params.require(:bill).permit(:name, :creditor, :balance_owed, :monthly_payment, :due_date, :user_id)
+        params.require(:bill).permit(:name, :creditor, :balance_owed, :monthly_payment, :due_date, :category_id)
     end
 end
