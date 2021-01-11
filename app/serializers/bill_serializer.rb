19db9262c912c40ff036e00db32b090d775779fd
Awilliams4321 <awilliams1234@live.com>
2021-01-10 21:33:54 -0700

    Adding my category model to my Bill serializer.

--- app/serializers/bill_serializer.rb
+++ app/serializers/bill_serializer.rb
@@ -1,4 +1,4 @@
 class BillSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :creditor, :balance_owed, :monthly_payment, :due_date, :category_id 
+  attributes :id, :name, :creditor, :balance_owed, :monthly_payment, :due_date, :category_id, :category 
 end
