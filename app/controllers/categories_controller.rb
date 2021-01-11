f27a928d932d21da04517811cbcb2a44b0524d55
Awilliams4321 <awilliams1234@live.com>
2021-01-11 08:18:16 -0700

    My categories contorller is no longer needed at the time being because i am accessing all categories THROUGH bills.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,28 @@
 class CategoriesController < ApplicationController
 
-    def index
-        categories = Category.all 
-        render json: CategorySerializer.new(categories)
-    end 
+    # def index
+    #     categories = Category.all 
+    #     render json: CategorySerializer.new(categories)
+    # end 
 
-    def show
-        category = Category.find_by(id: params[:id])
-        render json: CategorySerializer.new(category)
-    end
+    # def show
+    #     category = Category.find_by(id: params[:id])
+    #     render json: CategorySerializer.new(category)
+    # end
 
-    def create
-        category = Category.new(category_params)
-        if category.save
-            render json: CategorySerializer.new(category)
-        else
-            render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
-        end
-    end
+    # def create
+    #     category = Category.new(category_params)
+    #     if category.save
+    #         render json: CategorySerializer.new(category)
+    #     else
+    #         render json: {errors: category.errors.full_messages}, status: :unprocessible_entity
+    #     end
+    # end
 
-    private
-    def category_params
-        params.require(:category).permit(:name)
-    end
+    # private
+    # def category_params
+    #     params.require(:category).permit(:name)
+    # end
 
 
 end
