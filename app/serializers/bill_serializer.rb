fdac2033d6b3e20606f107e8a1a9e1cf2f97f09f
Awilliams4321 <awilliams1234@live.com>
2021-01-13 09:34:06 -0700

    No changes were made.

--- app/serializers/bill_serializer.rb
+++ app/serializers/bill_serializer.rb
@@ -1,4 +1,4 @@
 class BillSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :creditor, :balance_owed, :monthly_payment, :due_date, :category_id, :category 
+  attributes :name, :creditor, :balance_owed, :monthly_payment, :due_date, :category_id, :category 
 end
